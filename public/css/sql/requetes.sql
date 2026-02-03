0671652c9fa561f60e482afbf3c2af67591609b4
naelchamssiddine-670 <naelchamssiddine@gmail.com>
2026-02-03 10:58:21 +0300

    ajoute de membre dans mon tableau d'equipe

--- public/css/sql/requetes.sql
+++ public/css/sql/requetes.sql
@@ -1,3 +1,9 @@
+-- Créer la base de données
+CREATE DATABASE maygourmet;
+-- Afficher les bases de données existantes
+SHOW DATABASES;
+
+
 CREATE TABLE equipe (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, -- Identifiant unique de l'équipe
     nom VARCHAR(155) NOT NULL, -- Nom de l'équipe
@@ -7,6 +13,24 @@ CREATE TABLE equipe (
     poste VARCHAR(80) NOT NULL, -- Poste occupé dans l'équipe
     adress_postale VARCHAR(250), -- Adresse postale de l'équipe
     presentation VARCHAR(255), -- Brève présentation de l'équipe
-    ville VARCHAR(100) NOT NULL,
-    date_creation DATE NOT NULL
+    date_recrutement DATE-- Date de recrutement de l'équipe    
 );
+
+-- Afficher les tables de la base de données
+SHOW TABLES;
+
+-- Ajouter un membre dans l'équipe
+INSERT INTO equipe (nom, prenom, mail, telephone, poste, adress_postale, presentation, date_recrutement)
+VALUES ("ZANFARANE","HAMZA","zanfaranehamza@gmail.com","0639123456","Gérant","4 rue de la mosquée 97600 Mamoudzou","Passionné de cuisine traditionnelle et moderne.", "2023-10-01");
+
+INSERT INTO equipe (nom, prenom, mail, telephone, poste, adress_postale, presentation, date_recrutement)
+VALUES ("MOHAMED","ISSA","mohamedissa@gmail.com","0639123456","chef cuisinier","45 rue maweni 97640 sada","Expert en cuisine locale et internationale.", "2023-11-15");
+
+INSERT INTO equipe (nom, prenom, mail, telephone, poste, adress_postale, presentation, date_recrutement)
+VALUES ("ALI","FATIMA","alifatima@gamil.com","0639123456","Responsable des commandes","12 avenue de la plage 97600 Mamoudzou","Organisée et efficace dans la gestion des approvisionnements.", "2024-01-20");
+
+INSERT INTO equipe (nom, prenom, mail, telephone, poste, adress_postale, presentation, date_recrutement)
+VALUES ("ABDULLAHI","YOUSSOUF","abdullahi@gmail.com","0639123456","Serveur","78 boulevard de la mer 97600 Mamoudzou","Aimable et attentif au service des clients.", "2024-03-10");
+
+INSERT INTO equipe (nom, prenom, mail, telephone, poste, adress_postale, presentation, date_recrutement)
+VALUES ("HASSANI","AICHA","hassani@gmail.com","0639123456","Responsable marketing","23 rue des fleurs 97600 Mamoudzou","Créative et passionnée par la promotion de la cuisine.", "2024-05-05");
